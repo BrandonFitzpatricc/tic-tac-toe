07a34a4141c70c34f5d32cffce859e089607960b
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-02-06 10:39:46 -0500

    Write gameBoard module

---
+++ scripts/javascript.js
@@ -0,0 +1,44 @@
+const gameBoard = (function() {
+    const board = [
+                    ["", "", ""],
+                    ["", "", ""], 
+                    ["", "", ""]
+                  ];
+
+    const getBoard = () => board;
+
+    const threeInARow = () => {
+        let row;
+        let column;
+        let leftDiagonal = [];
+        let rightDiagonal = [];
+
+        let threeInARow;
+
+        for(let i = 0; i < board.length; i++) {
+            row = board[i];
+            column = [];
+            
+            for(let j = 0; j < board.length; j++) {
+                column.push(board[j][i]);
+            }
+
+            for(const arr of [row, column]) {
+                threeInARow = arr.every(cell => cell === arr[0] && arr[0] !== "");
+                if(threeInARow) return true;
+            }
+
+            leftDiagonal.push(board[i][i]);
+            rightDiagonal.push(board[i][(board.length - 1) - i]);
+        }
+
+        for(const arr of [leftDiagonal, rightDiagonal]) {
+            threeInARow = arr.every(cell => cell === arr[0] && arr[0] !== "");
+            if(threeInARow) return true;
+        }
+
+        return false;
+    }
+
+    return {getBoard, threeInARow};
+})();
